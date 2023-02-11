89d5bdf9cac6061757f69095e54cf9dbcec7ecd2
TakenouchiTR <62298020+TakenouchiTR@users.noreply.github.com>
2023-02-11 18:22:33 -0500

    Fix tabbing
    Replaced tab characters with spaces to keep tabbing consistent.

--- code/RecipeHub/Database/Passwords.sql
+++ code/RecipeHub/Database/Passwords.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Passwords]
 (
-	[userId] INT NOT NULL PRIMARY KEY, 
-  [password] NCHAR(256) NOT NULL,
-	CONSTRAINT [FK_Passwords_To_Users] FOREIGN KEY (userId) REFERENCES [Users]([userId])
+    [userId] INT NOT NULL PRIMARY KEY, 
+    [password] NCHAR(256) NOT NULL,
+    CONSTRAINT [FK_Passwords_To_Users] FOREIGN KEY (userId) REFERENCES [Users]([userId])
 )
