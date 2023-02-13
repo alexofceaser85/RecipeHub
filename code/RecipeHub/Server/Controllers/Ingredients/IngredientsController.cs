690ecd0176ab4d229536e256dabbd007145b093b
Alexander Ayers <alexayers2000@gmail.com>
2023-02-13 10:45:39 -0500

    Added the constructors for the controller.
    Added the constructors, to add the service to the ingredients controller.

--- code/RecipeHub/Server/Controllers/Ingredients/IngredientsController.cs
+++ code/RecipeHub/Server/Controllers/Ingredients/IngredientsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Server.Service.Ingredients;
 
 namespace Server.Controllers.Ingredients
 {
@@ -8,5 +9,32 @@ namespace Server.Controllers.Ingredients
     [ApiController]
     public class IngredientsController
     {
+        private readonly IIngredientsService service;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IngredientsController"/> class.<br />
+        /// <br />
+        /// Precondition: None<br />
+        /// Postcondition: All fields have been initialized to default values.
+        /// </summary>
+        public IngredientsController()
+        {
+            this.service = new IngredientsService();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IngredientsController"/> class.<br />
+        /// <br />
+        /// Precondition: service != null.<br />
+        /// Postcondition: All fields have been set to specified values.<br />
+        /// </summary>
+        /// <param name="service">The service to use for the different API routes.</param>
+        /// <exception cref="ArgumentNullException">If service is null.</exception>
+        public IngredientsController(IIngredientsService service)
+        {
+            this.service = service ?? throw new ArgumentNullException(nameof(service));
+        }
+
+
     }
 }
