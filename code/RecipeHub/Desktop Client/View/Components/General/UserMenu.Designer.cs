03fbb58ff3d7313de67a5d65d8544f486245e460
Alexander Ayers <alexayers2000@gmail.com>
2023-02-13 19:34:11 -0500

    Added inital user menu.

--- code/RecipeHub/Desktop Client/View/Components/General/UserMenu.Designer.cs
+++ code/RecipeHub/Desktop Client/View/Components/General/UserMenu.Designer.cs
@@ -28,10 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.panel1.Location = new System.Drawing.Point(292, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(46, 10);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Desktop;
+            this.panel2.Location = new System.Drawing.Point(292, 19);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(46, 10);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.Desktop;
+            this.panel3.Location = new System.Drawing.Point(292, 35);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(46, 10);
+            this.panel3.TabIndex = 2;
+            // 
+            // UserMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "UserMenu";
+            this.Size = new System.Drawing.Size(341, 653);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
     }
 }
