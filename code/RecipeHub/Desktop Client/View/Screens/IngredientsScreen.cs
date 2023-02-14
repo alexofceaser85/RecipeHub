97741ea78b930b3c829b7dd3103597763220670f
Alexander Ayers <alexayers2000@gmail.com>
2023-02-13 19:32:23 -0500

    Added initial UI features.

---
+++ code/RecipeHub/Desktop Client/View/Screens/IngredientsScreen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Desktop_Client.View.Screens
+{
+    public partial class IngredientsScreen : Form
+    {
+        public IngredientsScreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
