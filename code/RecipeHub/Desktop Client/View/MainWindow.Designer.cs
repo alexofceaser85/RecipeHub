90dc2236015ed423e96ede7c43cb430b72954451
TakenouchiTR <62298020+TakenouchiTR@users.noreply.github.com>
2023-02-14 16:10:47 -0500

    Update the title bar
    Updates the text on the title bar to match the current application.

--- code/RecipeHub/Desktop Client/View/MainWindow.Designer.cs
+++ code/RecipeHub/Desktop Client/View/MainWindow.Designer.cs
@@ -37,7 +37,8 @@
             this.ClientSize = new System.Drawing.Size(691, 1268);
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "MainWindow";
-            this.Text = "NurseView";
+            this.Text = "RecipeHub";
+            this.Resize += new System.EventHandler(this.MainWindow_Resize);
             this.ResumeLayout(false);
 
         }
