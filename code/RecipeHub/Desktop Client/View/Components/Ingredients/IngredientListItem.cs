020aa2565ff6da00b9937df3ed9131c6041da237
TakenouchiTR <62298020+TakenouchiTR@users.noreply.github.com>
2023-02-22 18:17:05 -0500

    Replace ingredient screen DataGridView with list
    Replaces the `DataGridView` that was originally used for the list of ingredients with a table containing the newly created `IngredientListItem`. This brings the appearance of the ingredients list more inline with the rest of the UI by changing the font used and the color of the buttons.

---
+++ code/RecipeHub/Desktop Client/View/Components/Ingredients/IngredientListItem.cs
@@ -0,0 +1,47 @@
+﻿using Shared_Resources.Model.Ingredients;
+using Shared_Resources.Utils.Units;
+
+namespace Desktop_Client.View.Components.Ingredients
+{
+    /// <summary>
+    /// An item to display on a list of ingredients
+    /// </summary>
+    public partial class IngredientListItem : UserControl
+    {
+        public Ingredient Ingredient { get; set; }
+
+        public IngredientListItem(Ingredient ingredient)
+        {
+            this.InitializeComponent();
+
+            this.ingrediantNameLabel.Text = ingredient.Name;
+            this.quantityLabel.Text = @$"Quantity: {ingredient.Amount} {BaseUnitUtils.GetBaseUnitSign(ingredient.MeasurementType)}";
+            this.Ingredient = ingredient;
+        }
+
+        /// <summary>
+        /// Occurs when the edit button is pressed
+        /// </summary>
+        public EventHandler<Ingredient>? EditSelected;
+
+        /// <summary>
+        /// Occurs when the remove button is pressed
+        /// </summary>
+        public EventHandler<Ingredient>? RemoveSelected;
+
+        private void editButton_Click(object sender, EventArgs e)
+        {
+            this.EditSelected?.Invoke(this, this.Ingredient);
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            this.RemoveSelected?.Invoke(this, this.Ingredient);
+        }
+
+        private void ingrediantNameLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
