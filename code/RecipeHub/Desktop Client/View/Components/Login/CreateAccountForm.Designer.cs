fa77900822d202045b81a88a24d16a61930b85ad
TakenouchiTR <62298020+TakenouchiTR@users.noreply.github.com>
2023-02-25 15:03:24 -0500

    Remove unnecessary event handler
    Removes an unnecessary event handler that was automatically added by Windows Forms.

--- code/RecipeHub/Desktop Client/View/Components/Login/CreateAccountForm.Designer.cs
+++ code/RecipeHub/Desktop Client/View/Components/Login/CreateAccountForm.Designer.cs
@@ -100,7 +100,6 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel1.Size = new System.Drawing.Size(509, 1000);
             this.tableLayoutPanel1.TabIndex = 0;
-            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
             // 
             // emailErrorLabel
             // 
