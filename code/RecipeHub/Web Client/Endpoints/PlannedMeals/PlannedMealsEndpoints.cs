a316b1de755798005ad6e2708b48023dc860fcb7
Alexander Ayers <alexayers2000@gmail.com>
2023-03-12 19:56:35 -0400

    Added initial services to web.
    Added the intial services, endpoints, and viewmodel for the web and planned meals.

---
+++ code/RecipeHub/Web Client/Endpoints/PlannedMeals/PlannedMealsEndpoints.cs
@@ -0,0 +1,98 @@
+﻿using System.Text.Json;
+using Shared_Resources.Data.Settings;
+using Shared_Resources.Data.UserData;
+using Shared_Resources.Model.PlannedMeals;
+using Shared_Resources.Utils.Json;
+using Shared_Resources.Utils.Server;
+
+namespace Web_Client.Endpoints.PlannedMeals
+{
+    /// <inheritdoc cref="IPlannedMealsEndpoints"/>
+    public class PlannedMealsEndpoints : IPlannedMealsEndpoints
+    {
+        private const string AddPlannedMealsRoute = "AddPlannedMeal";
+        private const string RemovePlannedMealsRoute = "RemovePlannedMeal";
+        private const string GetPlannedMealsRoute = "GetPlannedMeals";
+
+        private const string PlannedMealsElementName = "plannedMeals";
+
+        private readonly HttpClient client;
+
+        /// <summary>
+        /// Creates a default instance of <see cref="PlannedMealsEndpoints"/>, using a default <see cref="HttpClient"/>.<br/>
+        /// <br/>
+        /// <b>Precondition: </b>None<br/>
+        /// <b>Postcondition: </b>None
+        /// </summary>
+        public PlannedMealsEndpoints() : this(new HttpClient())
+        {
+
+        }
+
+        /// <summary>
+        /// Creates an instance of <see cref="PlannedMealsEndpoints"/> with a specified <see cref="HttpClient"/>.<br/>
+        /// <br/>
+        /// <b>Precondition: </b>client != null<br/>
+        /// <b>Postcondition: </b>None
+        /// </summary>
+        /// <param name="client"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public PlannedMealsEndpoints(HttpClient client)
+        {
+            this.client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        /// <summary>
+        /// Adds a recipe to the active user's planned meals, using a specified <see cref="DateTime"/> and <see cref="MealCategory"/>.<br/>
+        /// <br/>
+        /// <b>Precondition: </b>None<br/>
+        /// <b>Postcondition: </b>None
+        /// </summary>
+        /// <param name="mealDate">The date to add the meal</param>
+        /// <param name="category">The category of the meal</param>
+        /// <param name="recipeId">The recipe to add to the meal</param>
+        public void AddPlannedMeal(DateTime mealDate, MealCategory category, int recipeId)
+        {
+            var serverMethodParameters = $"?sessionKey={Session.Key}&mealDate={mealDate}&category={category}&recipeId={recipeId}";
+            var requestUri = $"{ServerSettings.ServerUri}{AddPlannedMealsRoute}{serverMethodParameters}";
+            var json = ServerUtils.RequestJson(HttpMethod.Post, requestUri, this.client);
+            JsonUtils.VerifyAndGetRequestInfo(json);
+        }
+
+        /// <summary>
+        /// Removes a recipe to the active user's planned meals, using a specified <see cref="DateTime"/> and <see cref="MealCategory"/>.<br/>
+        /// <br/>
+        /// <b>Precondition: </b>None<br/>
+        /// <b>Postcondition: </b>None
+        /// </summary>
+        /// <param name="mealDate">The date to remove the meal</param>
+        /// <param name="category">The category of the meal</param>
+        /// <param name="recipeId">The recipe to remove from the meal</param>
+        public void RemovePlannedMeal(DateTime mealDate, MealCategory category, int recipeId)
+        {
+            var serverMethodParameters = $"?sessionKey={Session.Key}&mealDate={mealDate}&category={category}&recipeId={recipeId}";
+            var requestUri = $"{ServerSettings.ServerUri}{RemovePlannedMealsRoute}{serverMethodParameters}";
+            var json = ServerUtils.RequestJson(HttpMethod.Post, requestUri, this.client);
+            JsonUtils.VerifyAndGetRequestInfo(json);
+        }
+
+        /// <summary>
+        /// Gets all of the planned meals for the active user.<br/>
+        /// <br/>
+        /// <b>Precondition: </b>None<br/>
+        /// <b>Postcondition: </b>None
+        /// </summary>
+        /// <returns>All of the user's planned meals.</returns>
+        public PlannedMeal[] GetPlannedMeals()
+        {
+            var serverMethodParameters = $"?sessionKey={Session.Key}";
+            var requestUri = $"{ServerSettings.ServerUri}{GetPlannedMealsRoute}{serverMethodParameters}";
+            var json = ServerUtils.RequestJson(HttpMethod.Get, requestUri, this.client);
+            JsonUtils.VerifyAndGetRequestInfo(json);
+
+            var meals = json[PlannedMealsElementName]!.AsArray().Deserialize<PlannedMeal[]>();
+
+            return meals!;
+        }
+    }
+}
---
+++ code/RecipeHub/Web Client/ViewModel/PlannedMeals/PlannedMealsViewModel.cs
@@ -0,0 +1,111 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Shared_Resources.Model.PlannedMeals;
+using Web_Client.Pages;
+using Web_Client.Service.PlannedMeals;
+
+namespace Web_Client.ViewModel.PlannedMeals
+{
+    /// <summary>
+    /// The view model for <see cref="PlannedMealsModel"/>
+    /// </summary>
+    public class PlannedMealsViewModel : INotifyPropertyChanged
+    {
+        private readonly IPlannedMealsService service;
+        private PlannedMeal[] plannedMeals;
+
+        /// <summary>
+        /// The array of planned meals to display
+        /// </summary>
+        public PlannedMeal[] PlannedMeals
+        {
+            get => this.plannedMeals;
+            set => this.SetField(ref this.plannedMeals, value);
+        }
+
+        /// <summary>
+        /// Creates a default instance of <see cref="PlannedMealsViewModel"/>, using a default <see cref="PlannedMealsService"/>.<br/>
+        /// <br/>
+        /// <b>Precondition: </b>None<br/>
+        /// <b>Postcondition: </b>this.PlannedMeals.Length == 0
+        /// </summary>
+        public PlannedMealsViewModel() : this(new PlannedMealsService())
+        {
+
+        }
+
+        /// <summary>
+        /// Creates an instance of <see cref="PlannedMealsViewModel"/> with a specified <see cref="IPlannedMealsService"/>.<br/>
+        /// <br/>
+        /// <b>Precondition: </b>service != null<br/>
+        /// <b>Postcondition: </b>this.PlannedMeals.Length == 0
+        /// </summary>
+        /// <param name="service">The planned meal service</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public PlannedMealsViewModel(IPlannedMealsService service)
+        {
+            this.service = service ?? throw new ArgumentNullException(nameof(service));
+            this.plannedMeals = Array.Empty<PlannedMeal>();
+        }
+
+        /// <summary>
+        /// Gets the planned meals for the current user.<br/>
+        /// <br/>
+        /// <b>Precondition: </b>None<br/>
+        /// <b>Postcondition: </b>this.PlannedMeals contains all planned meals for the active user
+        /// </summary>
+        public void Initialize()
+        {
+            this.PlannedMeals = this.service.GetPlannedMeals();
+        }
+
+        /// <summary>
+        /// Removes a recipe from the active user's planned meals.<br/>
+        /// <br/>
+        /// <b>Precondition: </b>None
+        /// <b>Postcondition: </b>The recipe is removed from this.PlannedMeals
+        /// </summary>
+        /// <param name="mealDate">The date for the planned meal</param>
+        /// <param name="category">The category of the planned meal</param>
+        /// <param name="recipeId">The id of the recipe to remove</param>
+        public void RemovePlannedMeal(DateTime mealDate, MealCategory category, int recipeId)
+        {
+            this.service.RemovePlannedMeal(mealDate, category, recipeId);
+        }
+
+        /// <inheritdoc/>
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        /// <summary>
+        /// Fires this.PropertyChanged with the specified property name.<br/>
+        /// Defaults to the name of the calling member, allowing for easier use within property bodies.<br/>
+        /// <br/>
+        /// <b>Precondition: </b>None<br/>
+        /// <b>Postcondition: </b>None
+        /// </summary>
+        /// <param name="propertyName">The name of the property that changed.</param>
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Updates a specified field, returning whether or not the value was changed.<br/>
+        /// <br/>
+        /// <b>Precondition: </b>None<br/>
+        /// <b>Postcondition: </b>this.[field] == value
+        /// </summary>
+        /// <typeparam name="T">The type of the property that changed.</typeparam>
+        /// <param name="field">The field that stores the data for the property</param>
+        /// <param name="value">The new value for the field</param>
+        /// <param name="propertyName">The name of the property that is being updated.</param>
+        /// <returns>Whether the value of the field has changed.</returns>
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
