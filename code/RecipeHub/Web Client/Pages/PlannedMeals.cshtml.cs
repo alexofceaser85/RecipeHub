9bbb98c1b4c62099652ea0a03229282caf807b84
Alexander Ayers <alexayers2000@gmail.com>
2023-03-14 01:24:41 -0400

    Added planned recipe page minus remove functionality.
    Added planned recipe page minus the functionality to remove a recipe from the planned meal.

--- code/RecipeHub/Web Client/Pages/PlannedMeals.cshtml.cs
+++ code/RecipeHub/Web Client/Pages/PlannedMeals.cshtml.cs
@@ -1,10 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Web_Client.ViewModel.PlannedMeals;
 
 namespace Web_Client.Pages
 {
+    /// <summary>
+    /// Model for the Planned Meals Page.
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNetCore.Mvc.RazorPages.PageModel" />
     public class PlannedMealsModel : PageModel
     {
+        public PlannedMealsViewModel ViewModel { get; set; }
+
+        public PlannedMealsModel()
+        {
+            this.ViewModel= new PlannedMealsViewModel();
+            this.ViewModel.Initialize();
+        }
+
+        /// <summary>
+        /// Called when [get].
+        /// </summary>
         public void OnGet()
         {
         }
