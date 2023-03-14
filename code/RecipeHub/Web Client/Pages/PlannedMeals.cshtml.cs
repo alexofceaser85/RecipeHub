fc258e0ff4ed9a86a522dae90d0f325703584157
Alexander Ayers <alexayers2000@gmail.com>
2023-03-14 01:58:34 -0400

    Added dialog and initial function for removing planned meal.
    Added in the dialog, with necessary values being loaded into the form, with a handler on the code-behind.

--- code/RecipeHub/Web Client/Pages/PlannedMeals.cshtml.cs
+++ code/RecipeHub/Web Client/Pages/PlannedMeals.cshtml.cs
@@ -10,8 +10,17 @@ namespace Web_Client.Pages
     /// <seealso cref="Microsoft.AspNetCore.Mvc.RazorPages.PageModel" />
     public class PlannedMealsModel : PageModel
     {
+        /// <summary>
+        /// Gets or sets the view model.
+        /// </summary>
+        /// <value>
+        /// The view model.
+        /// </value>
         public PlannedMealsViewModel ViewModel { get; set; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PlannedMealsModel"/> class.
+        /// </summary>
         public PlannedMealsModel()
         {
             this.ViewModel= new PlannedMealsViewModel();
@@ -23,6 +32,16 @@ namespace Web_Client.Pages
         /// </summary>
         public void OnGet()
         {
+        }
+
+        /// <summary>
+        /// Called when [post remove planned meal].
+        /// </summary>
+        /// <returns>a redirect to the planned meals page.</returns>
+        public IActionResult OnPostRemovePlannedMeal()
+        {
+            var form = Request.Form;
+            return RedirectToPage("PlannedMeals");
         }
     }
 }
