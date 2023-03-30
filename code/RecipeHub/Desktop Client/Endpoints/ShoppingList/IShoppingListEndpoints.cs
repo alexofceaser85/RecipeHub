65fb97dc457ab49605712136ea569b4557ef2be9
TakenouchiTR <62298020+TakenouchiTR@users.noreply.github.com>
2023-03-30 19:02:23 -0400

    Add endpoints for Shopping List
    Adds the endpoints for Shopping List, allowing the client to query the server for the active user's Shopping List.

---
+++ code/RecipeHub/Desktop Client/Endpoints/ShoppingList/IShoppingListEndpoints.cs
@@ -0,0 +1,20 @@
+﻿using Shared_Resources.Model.Ingredients;
+
+namespace Desktop_Client.Endpoints.ShoppingList
+{
+    /// <summary>
+    /// The interface for the recipe types endpoints
+    /// </summary>
+    public interface IShoppingListEndpoints
+    {
+        /// <summary>
+        /// Gets the shopping list for the current user.<br/>
+        /// <br/>
+        /// <b>Precondition: </b>None<br/>
+        /// <b>Postcondition: </b>None
+        /// </summary>
+        /// <returns>The shopping list for the current active user.</returns>
+        public Ingredient[] GetShoppingList();
+
+    }
+}
---
+++ code/RecipeHub/Tests/DesktopClientTests/DesktopClient/Endpoints/Ingredients/IngredientsEndpointsTests/AddIngredientsTests.cs
@@ -0,0 +1,171 @@
+﻿using Moq.Protected;
+using Moq;
+using System.Net;
+using Desktop_Client.Endpoints.Ingredients;
+using Shared_Resources.ErrorMessages;
+using Shared_Resources.Model.Ingredients;
+
+namespace DesktopClientTests.DesktopClient.Endpoints.Ingredients.IngredientsEndpointsTests
+{
+    public class AddIngredientsTests
+    {
+        [Test]
+        public void SuccessfullyAddEmptyIngredientArray()
+        {
+            const string json = "{\"flag\": true, \"code\": 200, \"message\": \"Ingredients successfully added.\"}";
+
+            var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+            mockHttpMessageHandler
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent(json)
+                });
+
+            var httpClient = new HttpClient(mockHttpMessageHandler.Object);
+            var endpoints = new IngredientEndpoints(httpClient);
+
+            Assert.Multiple(() =>
+            {
+                var result = endpoints.AddIngredients(Array.Empty<Ingredient>());
+                Assert.That(result, Is.EqualTo(true));
+                mockHttpMessageHandler
+                    .Protected()
+                    .Verify<Task<HttpResponseMessage>>("SendAsync", Times.Once(),
+                        ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>());
+            });
+        }
+
+        [Test]
+        public void SuccessfullyAddSingleIngredient()
+        {
+            const string json = "{\"flag\": true, \"code\": 200, \"message\": \"Ingredients successfully added.\"}";
+
+            var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+            mockHttpMessageHandler
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent(json)
+                });
+
+            var httpClient = new HttpClient(mockHttpMessageHandler.Object);
+            var endpoints = new IngredientEndpoints(httpClient);
+
+            Assert.Multiple(() =>
+            {
+                var result = endpoints.AddIngredients(new Ingredient[] {new()});
+                Assert.That(result, Is.EqualTo(true));
+                mockHttpMessageHandler
+                    .Protected()
+                    .Verify<Task<HttpResponseMessage>>("SendAsync", Times.Once(),
+                        ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>());
+            });
+        }
+
+        [Test]
+        public void SuccessfullyAddManyIngredients()
+        {
+            const string json = "{\"flag\": true, \"code\": 200, \"message\": \"Ingredients successfully added.\"}";
+
+            var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+            mockHttpMessageHandler
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent(json)
+                });
+
+            var httpClient = new HttpClient(mockHttpMessageHandler.Object);
+            var endpoints = new IngredientEndpoints(httpClient);
+            var ingredients = new Ingredient[] {
+                new(),
+                new(),
+                new()
+            };
+
+            Assert.Multiple(() =>
+            {
+                var result = endpoints.AddIngredients(ingredients);
+                Assert.That(result, Is.EqualTo(true));
+                mockHttpMessageHandler
+                    .Protected()
+                    .Verify<Task<HttpResponseMessage>>("SendAsync", Times.Once(),
+                        ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>());
+            });
+        }
+
+        [Test]
+        public void UnsuccessfullyAddNewIngredient()
+        {
+            const string errorMessage = "error message";
+            const string json = $"{{\"code\": 500, \"message\": \"{errorMessage}\"}}";
+
+            var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+            mockHttpMessageHandler
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage {
+                    StatusCode = HttpStatusCode.InternalServerError,
+                    Content = new StringContent(json)
+                });
+
+            var httpClient = new HttpClient(mockHttpMessageHandler.Object);
+            var endpoints = new IngredientEndpoints(httpClient);
+
+            Assert.Multiple(() =>
+            {
+                var message = Assert.Throws<ArgumentException>(
+                    () => endpoints.AddIngredients(Array.Empty<Ingredient>()))!.Message;
+                Assert.That(message, Is.EqualTo(errorMessage));
+
+                mockHttpMessageHandler
+                    .Protected()
+                    .Verify<Task<HttpResponseMessage>>("SendAsync", Times.Once(),
+                        ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>());
+            });
+        }
+
+        [Test]
+        public void SessionKeyIsInvalid()
+        {
+            const string errorMessage = UsersServiceErrorMessages.UnauthorizedAccessErrorMessage;
+            const string json = $"{{\"code\": 401, \"message\": \"{errorMessage}\"}}";
+
+            var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+            mockHttpMessageHandler
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.InternalServerError,
+                    Content = new StringContent(json)
+                });
+
+            var httpClient = new HttpClient(mockHttpMessageHandler.Object);
+            var endpoints = new IngredientEndpoints(httpClient);
+
+            Assert.Multiple(() =>
+            {
+                var message = Assert.Throws<UnauthorizedAccessException>(
+                    () => endpoints.AddIngredients(Array.Empty<Ingredient>()))!.Message;
+                Assert.That(message, Is.EqualTo(errorMessage));
+
+                mockHttpMessageHandler
+                    .Protected()
+                    .Verify<Task<HttpResponseMessage>>("SendAsync", Times.Once(),
+                        ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>());
+            });
+        }
+    }
+}
