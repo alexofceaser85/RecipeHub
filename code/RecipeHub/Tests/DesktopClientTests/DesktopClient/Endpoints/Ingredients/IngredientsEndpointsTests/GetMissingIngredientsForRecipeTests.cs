610ba7b6b2b82922e2f34e2e22cd40e05e2309bf
TakenouchiTR <62298020+TakenouchiTR@users.noreply.github.com>
2023-03-31 19:30:17 -0400

    Implement getting missing ingredients, cook recipe
    Adds the option to get the active user's missing ingredients for a specified recipe and the ability to remove all of a recipe's ingredient's from the active user's pantry.
    
    Both methods will be used when the user is on a recipe screen, allowing them to mark a recipe as completed. If the user doesn't have enough ingredients to complete it, they will be informed about which ingredients they are missing.

---
+++ code/RecipeHub/Tests/DesktopClientTests/DesktopClient/Endpoints/Ingredients/IngredientsEndpointsTests/GetMissingIngredientsForRecipeTests.cs
@@ -0,0 +1,191 @@
+﻿using Moq.Protected;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Desktop_Client.Endpoints.Ingredients;
+using Shared_Resources.ErrorMessages;
+using Shared_Resources.Model.Ingredients;
+
+namespace DesktopClientTests.DesktopClient.Endpoints.Ingredients.IngredientsEndpointsTests
+{
+    internal class GetMissingIngredientsForRecipeTests
+    {
+        [Test]
+        public void GetEmptyArrayOfMissingIngredients()
+        {
+            const string json = "{\"ingredients\": [], \"code\": 200, " +
+                                "\"message\": \"Ingredients successfully retrieved.\"}";
+            const int recipeId = 0;
+
+            var ingredients = Array.Empty<Ingredient>();
+
+            var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+            mockHttpMessageHandler
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent(json)
+                });
+
+            var httpClient = new HttpClient(mockHttpMessageHandler.Object);
+            var endpoints = new IngredientEndpoints(httpClient);
+
+            Assert.Multiple(() =>
+            {
+                var result = endpoints.GetMissingIngredientsForRecipe(recipeId);
+                Assert.That(result, Is.EquivalentTo(ingredients));
+                mockHttpMessageHandler
+                    .Protected()
+                    .Verify<Task<HttpResponseMessage>>("SendAsync", Times.Once(),
+                        ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>());
+            });
+        }
+
+        [Test]
+        public void GetSingleMissingIngredient()
+        {
+            const string json = "{\"ingredients\": [{\"name\": \"apple\", \"amount\": 1, \"measurementType\": 0}], " +
+                                "\"code\": 200, \"message\": \"Ingredients successfully retrieved.\"}";
+            const int recipeId = 0;
+
+            var ingredients = new[] {
+                new Ingredient("apple", 1, 0)
+            };
+
+            var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+            mockHttpMessageHandler
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent(json)
+                });
+
+            var httpClient = new HttpClient(mockHttpMessageHandler.Object);
+            var endpoints = new IngredientEndpoints(httpClient);
+
+            Assert.Multiple(() =>
+            {
+                var result = endpoints.GetMissingIngredientsForRecipe(recipeId);
+                Assert.That(result, Is.EquivalentTo(ingredients));
+                mockHttpMessageHandler
+                    .Protected()
+                    .Verify<Task<HttpResponseMessage>>("SendAsync", Times.Once(),
+                        ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>());
+            });
+        }
+
+        [Test]
+        public void GetManyMissingIngredients()
+        {
+            const string json = "{\"ingredients\": [{\"name\": \"apple\", \"amount\": 1, \"measurementType\": 0}, " +
+                                "{\"name\": \"banana\", \"amount\": 1, \"measurementType\": 0}], \"code\": 200, " +
+                                "\"message\": \"Ingredients successfully retrieved.\"}";
+            const int recipeId = 0;
+
+            var ingredients = new[] {
+                new Ingredient("apple", 1, 0),
+                new Ingredient("banana", 1, 0)
+            };
+
+            var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+            mockHttpMessageHandler
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent(json)
+                });
+
+            var httpClient = new HttpClient(mockHttpMessageHandler.Object);
+            var endpoints = new IngredientEndpoints(httpClient);
+
+            Assert.Multiple(() =>
+            {
+                var result = endpoints.GetMissingIngredientsForRecipe(recipeId);
+                Assert.That(result, Is.EquivalentTo(ingredients));
+                mockHttpMessageHandler
+                    .Protected()
+                    .Verify<Task<HttpResponseMessage>>("SendAsync", Times.Once(),
+                        ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>());
+            });
+        }
+
+        [Test]
+        public void UnsuccessfullyGetMissingIngredients()
+        {
+            const string errorMessage = "error message";
+            const string json = $"{{\"code\": 500, \"message\": \"{errorMessage}\"}}";
+
+            var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+            mockHttpMessageHandler
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.InternalServerError,
+                    Content = new StringContent(json)
+                });
+
+            var httpClient = new HttpClient(mockHttpMessageHandler.Object);
+            var endpoints = new IngredientEndpoints(httpClient);
+
+            Assert.Multiple(() =>
+            {
+                var message = Assert.Throws<ArgumentException>(
+                    () => endpoints.GetMissingIngredientsForRecipe(0))!.Message;
+                Assert.That(message, Is.EqualTo(errorMessage));
+
+                mockHttpMessageHandler
+                    .Protected()
+                    .Verify<Task<HttpResponseMessage>>("SendAsync", Times.Once(),
+                        ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>());
+            });
+        }
+
+        [Test]
+        public void SessionKeyIsInvalid()
+        {
+            const string errorMessage = UsersServiceErrorMessages.UnauthorizedAccessErrorMessage;
+            const string json = $"{{\"code\": 401, \"message\": \"{errorMessage}\"}}";
+
+            var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+            mockHttpMessageHandler
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.InternalServerError,
+                    Content = new StringContent(json)
+                });
+
+            var httpClient = new HttpClient(mockHttpMessageHandler.Object);
+            var endpoints = new IngredientEndpoints(httpClient);
+
+            Assert.Multiple(() =>
+            {
+                var message = Assert.Throws<UnauthorizedAccessException>(
+                    () => endpoints.GetMissingIngredientsForRecipe(0))!.Message;
+                Assert.That(message, Is.EqualTo(errorMessage));
+
+                mockHttpMessageHandler
+                    .Protected()
+                    .Verify<Task<HttpResponseMessage>>("SendAsync", Times.Once(),
+                        ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>());
+            });
+        }
+    }
+}
