bbac6a27a8c9ff3f52b643edb17124dc5df4bd6e
Alexander Ayers <alexayers2000@gmail.com>
2023-04-01 18:11:49 -0400

    Added viewable shopping list.
    Added the shopping list view to the web.

--- code/RecipeHub/Web Client/Pages/ShoppingList.cshtml.cs
+++ code/RecipeHub/Web Client/Pages/ShoppingList.cshtml.cs
@@ -1,10 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Shared_Resources.Model.Ingredients;
+using Web_Client.ViewModel.ShoppingList;
 
 namespace Web_Client.Pages
 {
+    /// <summary>
+    /// Code-Behind for the Shopping List.
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNetCore.Mvc.RazorPages.PageModel" />
     public class ShoppingListModel : PageModel
     {
+        private ShoppingListViewModel viewModel;
+
+        /// <summary>
+        /// Gets the shopping list.
+        /// </summary>
+        /// <value>
+        /// The shopping list.
+        /// </value>
+        public Ingredient[] ShoppingList => this.viewModel.ShoppingList;
+
+        public ShoppingListModel()
+        {
+            this.viewModel = new ShoppingListViewModel();
+            this.viewModel.GetShoppingList();
+        }
+
+        /// <summary>
+        /// Called when the page is retrieved initially.<br />
+        /// <br />
+        /// Precondition: None<br />
+        /// Postcondition: None<br />
+        /// </summary>
         public void OnGet()
         {
         }
