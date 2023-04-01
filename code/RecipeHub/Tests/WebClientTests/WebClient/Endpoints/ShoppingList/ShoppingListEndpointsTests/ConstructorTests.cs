6a941514c69f644aaa483915a6c9fc71f778ac70
Alexander Ayers <alexayers2000@gmail.com>
2023-04-01 16:22:34 -0400

    Added test code for shopping list on web.
    Added the test code for endponts, services, and viewmodel on web.

---
+++ code/RecipeHub/Tests/WebClientTests/WebClient/Endpoints/ShoppingList/ShoppingListEndpointsTests/ConstructorTests.cs
@@ -0,0 +1,35 @@
+﻿using Web_Client.Endpoints.ShoppingList;
+
+namespace WebClientTests.WebClient.Endpoints.ShoppingList.ShoppingListEndpointsTests
+{
+    public class ConstructorTests
+    {
+        [Test]
+        public void ShouldCreateZeroParameterConstructor()
+        {
+            Assert.DoesNotThrow(() =>
+            {
+                _ = new ShoppingListEndpoints();
+            });
+        }
+
+        [Test]
+        public void ShouldCreateOneParameterConstructor()
+        {
+            Assert.DoesNotThrow(() =>
+            {
+                _ = new ShoppingListEndpoints(new HttpClient());
+            });
+        }
+
+        [Test]
+        public void ShouldNotCreateConstructorIfClientIsNull()
+        {
+            var message = Assert.Throws<ArgumentNullException>(() =>
+            {
+                _ = new ShoppingListEndpoints(null!);
+            })?.Message;
+            Assert.That(message, Is.EqualTo("Value cannot be null. (Parameter 'client')"));
+        }
+    }
+}
