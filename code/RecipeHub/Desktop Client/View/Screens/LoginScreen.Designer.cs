55a79e50e3e572d3a75f45e3b62f9b528006d0aa
TakenouchiTR <62298020+TakenouchiTR@users.noreply.github.com>
2023-04-12 16:47:03 -0400

    Center login form on screen
    Centers the login form on the screen.
    
    Previously, it was pushed down towards the bottom half of the screen.

--- code/RecipeHub/Desktop Client/View/Screens/LoginScreen.Designer.cs
+++ code/RecipeHub/Desktop Client/View/Screens/LoginScreen.Designer.cs
@@ -40,7 +40,7 @@
             this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Controls.Add(this.loginForm, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.loginForm, 0, 0);
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 1;
